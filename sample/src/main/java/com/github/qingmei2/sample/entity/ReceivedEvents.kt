2d7a6f7dbeabade636aa2db7a3e39d177ae8807a
qingmei2 <mq2553299@gmail.com>
2019-02-23 22:06:57 +0800

    Pref the SingletonHolder classes.

--- sample/src/main/java/com/github/qingmei2/sample/entity/ReceivedEvents.kt
+++ sample/src/main/java/com/github/qingmei2/sample/entity/ReceivedEvents.kt
@@ -2,27 +2,29 @@ package com.github.qingmei2.sample.entity
 
 import com.google.gson.annotations.SerializedName
 
-data class ReceivedEvent(val id: String,
-                         val type: Type,
-                         val actor: Actor,
-//                         val payload: Payload,
-                         val repo: ReceivedEventRepo,
-                         val public: Boolean,
-                         @SerializedName("created_at") val createdAt: String?)
+data class ReceivedEvent(
+    val id: String,
+    val type: Type,
+    val actor: Actor,
+    val repo: ReceivedEventRepo,
+    val public: Boolean,
+    @SerializedName("created_at") val createdAt: String?
+)
 
-data class Actor(val id: Int,
-                 val login: String,
-                 @SerializedName("display_login") val displayLogin: String,
-                 @SerializedName("gravatar_id") val gravatarId: String,
-                 val url: String,
-                 @SerializedName("avatar_url") val avatarUrl: String)
+data class Actor(
+    val id: Int,
+    val login: String,
+    @SerializedName("display_login") val displayLogin: String,
+    @SerializedName("gravatar_id") val gravatarId: String,
+    val url: String,
+    @SerializedName("avatar_url") val avatarUrl: String
+)
 
-data class ReceivedEventRepo(val id: String,
-                             val name: String,
-                             val url: String)
-
-data class Payload(val action: String?,
-                   val forkee: String?)
+data class ReceivedEventRepo(
+    val id: String,
+    val name: String,
+    val url: String
+)
 
 
 enum class Type {
